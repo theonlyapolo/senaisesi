f260c18b390e6ecdd4595a81de979d77d6d6c7eb
Apolo <paolacrisnvs@gmail.com>
2025-05-29 17:20:02 -0300

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))  # DON'T CHANGE THIS !!!
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
